29a83b112991c3544368c6c8f21c6707e39a46d1
Ahmed moh abd <ahmed.moh.abd.911@gmail.com>
2026-08-03 07:12:57 +0300

    add the buzzer instead of red light !

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,15 +4,15 @@ float Celsieus ;
 bool pass_corr = false ;
 
 const int temp_pin = A0 ; 
-const int RED_LED = 13 ; 
-const int GREEN_LED = 12 ; 
-const int WHITE_LED = 11 ; 
+const int BUZZER = 11 ; 
+const int GREEN_LED = 13 ; 
+const int WHITE_LED = 12 ; 
 
 
 void setup() {
 
 Serial.begin(9600);
-pinMode(RED_LED , OUTPUT);
+pinMode(BUZZER , OUTPUT);
 pinMode(GREEN_LED , OUTPUT);
 pinMode(WHITE_LED , OUTPUT);
 pinMode(temp_pin, INPUT);
@@ -24,15 +24,36 @@ void loop() {
 if (Serial.available() > 0) {
   String command  = Serial.readStringUntil('\n') ; 
   command.trim();
+
   if (command == "PASSWORD_OK" ) { 
-    digitalWrite(RED_LED , HIGH) ; 
+    digitalWrite(BUZZER , HIGH) ; 
+    delay(1000);
+    digitalWrite(BUZZER , LOW) ; 
+    delay(500);
+    digitalWrite(BUZZER , HIGH) ; 
+    delay(1000);
+    digitalWrite(BUZZER , LOW) ; 
+    delay(500);
+    digitalWrite(BUZZER , HIGH) ; 
+    delay(1000);
+    digitalWrite(BUZZER , LOW) ; 
     pass_corr = true; } 
-  else if(command == "PASSWORD_FAIL" && pass_corr == false )  digitalWrite(RED_LED , LOW) ; // must be of streamlit part to not let the user add audio when in commands tab !
+
+  else if (command == "PASSWORD_FAIL") {  
+      digitalWrite(BUZZER, LOW); 
+      pass_corr = false; 
+  }  
+
   else if (pass_corr) {
+     
       if      (command == "LIGHT_ON" )       digitalWrite(WHITE_LED , HIGH); 
+     
       else if (command == "LIGHT_OFF")       digitalWrite(WHITE_LED , LOW);
+     
       else if (command == "MUSIC_ON" )       digitalWrite(GREEN_LED , HIGH);
+     
       else if (command == "MUSIC_OFF")       digitalWrite(GREEN_LED , LOW);
+     
       else if (command == "SEND_TEMP")  {
         float temp = temprature();
         Serial.print("Tempratute: ");
@@ -40,6 +61,7 @@ if (Serial.available() > 0) {
         Serial.println(" C");
         
       }
+     
       else Serial.print("Not a command !!");
 
 }
